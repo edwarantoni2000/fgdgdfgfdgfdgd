b0bc10a2395fccccc5d37aa4dde77493d343cd63
edwarantoni2000 <edwarantoni2000@gmail.com>
2021-09-30 10:56:00 +0700

    update

---
+++ Assets/Scripts/Player/Boost.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    GameObject player;
+    GameObject a;
+    PlayerMovement playermove;
+
+    private void Awake()
+    {
+        a = GameObject.FindGameObjectWithTag("Boost");
+        player = GameObject.FindGameObjectWithTag("Player");
+        playermove = player.GetComponent<PlayerMovement>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Set player in range
+        if (other.gameObject == player && other.isTrigger == false)
+        {
+            playermove.speede(5f);
+            Destroy(a);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Live.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class Live : MonoBehaviour
+{
+    GameObject player;
+    GameObject a;
+    PlayerHealth playerHealth;
+
+    private void Awake()
+    {
+        a = GameObject.FindGameObjectWithTag("Live");
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Set player in range
+        if (other.gameObject == player && other.isTrigger == false)
+        {
+            playerHealth.Live();
+            Destroy(a);
+        }
+    }
+}
